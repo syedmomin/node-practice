479165e0206f2cb04163f1abea649516f22f89b3
Syed Momin <s.momin@finspacegroup.co.uk>
2022-12-14 20:03:37 +0500

    server update

--- node practice/index.mjs
+++ node practice/index.mjs
@@ -1,9 +1,12 @@
 import express  from "express"
 const app = express()
-const port = 3000
+const port = 3000  
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log("request ip :",req.ip);
+  let getTime = new Date();
+  let nowTime = `hour  ${getTime.getHours()} minute   ${getTime.getMinutes()} second   ${getTime.getSeconds()}`;
+  res.send('Hello User! Now Time is -->'+nowTime)
 })
 
 app.listen(port, () => {
